f586722cbd4384027d30b9a03457dd40dadbbc82
Philip Hartout <philip.hartout@protonmail.com>
2022-09-29 10:28:49 +0200

    fix nod evaluator

--- src/mhciipresentation/evaluation/nod.py
+++ src/mhciipresentation/evaluation/nod.py
@@ -7,42 +7,16 @@ This script validates our model against datasets from NOD mice
 """
 
 import argparse
-import copy
-import json
-import pprint
-import random
-from typing import Dict, List, Tuple
 
-import numpy as np
-import pandas as pd
 import torch
-from Bio.SeqIO.FastaIO import SimpleFastaParser
 from torch import nn
-from tqdm import tqdm
 
 from mhciipresentation.constants import AA_TO_INT, USE_GPU
 from mhciipresentation.inference import setup_model
-from mhciipresentation.loaders import (
-    load_internal_mouse_data,
-    load_K562_dataset,
-    load_melanoma_dataset,
-    load_pseudosequences,
-    load_uniprot,
-)
-from mhciipresentation.paths import (
-    DATA_DIR,
-    EPITOPES_DIR,
-    RAW_DATA,
-    SPLITS_DIR,
-)
-from mhciipresentation.transformer import (
-    TransformerModel,
-    evaluate_transformer,
-)
+from mhciipresentation.loaders import load_nod_data, load_nod_idx
 from mhciipresentation.utils import (
     compute_performance_measures,
     encode_aa_sequences,
-    flatten_lists,
     make_predictions_with_transformer,
     render_roc_curve,
     set_pandas_options,
@@ -62,15 +36,25 @@ def handle_public_NOD(
         device (torch.device): device to run the model on.
         batch_size (int): batch size to use for inference
     """
-    in_dir = SPLITS_DIR + "/mouse/" + "/random/"
-    test_data = pd.read_csv(in_dir + "X_test.csv")
-    test_data = test_data.loc[test_data["Peptide Sequence"].str.len() <= 25]
+    nod_data = load_nod_data()
+    _, _, X_test_idx = load_nod_idx()
+    test_data = nod_data.iloc[X_test_idx["index"]]
 
-    X = encode_aa_sequences(test_data["Peptide Sequence"], AA_TO_INT,)
+    if FLAGS.model_with_pseudo_path is not None:
+        X = encode_aa_sequences(
+            test_data.peptides_and_pseudosequence,
+            AA_TO_INT,
+        )
+    else:
+        X = encode_aa_sequences(
+            test_data["Peptide Sequence"],
+            AA_TO_INT,
+        )
+
     y = test_data.label.values
 
     predictions = make_predictions_with_transformer(
-        X, y, batch_size, device, model, input_dim, AA_TO_INT["X"]
+        X, batch_size, device, model, input_dim, AA_TO_INT["X"]
     )
     performance = compute_performance_measures(predictions, y)
     print(performance)
@@ -92,12 +76,12 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     "--model_with_pseudo_path",
-    #     "-modp",
-    #     type=str,
-    #     help="Path to the checkpoint of the model to evaluate.",
-    # )
+    parser.add_argument(
+        "--model_with_pseudo_path",
+        "-modp",
+        type=str,
+        help="Path to the checkpoint of the model to evaluate.",
+    )
     parser.add_argument(
         "--model_wo_pseudo_path",
         "-modwop",
