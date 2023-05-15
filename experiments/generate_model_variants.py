8dddc736b146c2b70a31040839a09e861a166aec
Philip Hartout <philip.hartout@protonmail.com>
2023-05-15 16:00:45 +0200

    rename model variant generating script and minor modifications for final training

--- experiments/generate_model_variants.py
+++ experiments/generate_model_variants.py
@@ -1,41 +1,35 @@
 # -*- coding: utf-8 -*-
-"""generate_experiments.py
+"""generate_model_variants.py
 
-This script generates the various configurations of gae experiments.
+This script generates the various configurations of aegis configurations.
 
 """
 
-import hashlib
+import itertools
 import os
 from pathlib import Path
 
-from pyprojroot import here
 
-
-def write_job(script_contents, fname):
-    with open(
-        Path("/fs/pool/pool-hartout/Documents/Git/AEGIS")
-        / "experiments"
-        / "job_scripts"
-        / fname,
-        "w",
-    ) as f:
+def write_job(script_contents, path_to_scripts, fname):
+    with open(path_to_scripts / fname, "w", encoding="utf-8") as f:
         f.write(script_contents)
 
 
-def build_job(overrides):
+def build_job(overrides, path_to_scripts):
     script_cotents = f"""#!/bin/bash -l
 ##############################
-#       Training ZINC        #
+#       Training AEGIS       #
 ##############################
 
 # Give your job a name, so you can recognize it in the queue overview
 #SBATCH --job-name=imm
-#SBATCH --cpus-per-task=19
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-core=2
 #SBATCH --mem-per-cpu=5G
 #SBATCH --gres=shard:19
 #SBATCH --time=20-00:00:00
 #SBATCH --partition=p.hpcl91
+#SBATCH --exclude=hpcl9104,hpcl9101
 #SBATCH --output=/fs/pool/pool-hartout/Documents/Git/AEGIS/outputs/slurm_outputs/slurm-%j.out
 source /fs/home/${"USER"}/.bashrc
 cd /fs/pool/pool-hartout/Documents/Git/AEGIS
@@ -50,14 +44,14 @@ exit 0
         "_".join(" ".join(overrides.split("=")).split(" ")[1:][::2][:-1])
         + ".sh"
     )
-    write_job(script_cotents, fname)
+    write_job(script_cotents, path_to_scripts, fname)
 
 
 def main():
     path_to_scripts = (
         Path("/fs/pool/pool-hartout/Documents/Git/AEGIS")
         / "experiments"
-        / "job_scripts"
+        / "variants"
     )
     filelist = [f for f in os.listdir(path_to_scripts) if f.endswith(".sh")]
     for f in filelist:
@@ -65,19 +59,21 @@ def main():
 
     feature_set = ["seq_only", "seq_mhc"]
     data_source = ["iedb", "iedb_nod", "nod"]
-    layers = [4, 8]
-    seeds = [0, 1, 2]
-    for seed in seeds:
-        for feat in feature_set:
-            for ds in data_source:
-                for layer in layers:
-                    build_job(
-                        f"dataset.data_source={ds} "
-                        + f"model.feature_set={feat} "
-                        + f"seed.seed={seed} "
-                        + f"model.aegis.n_layers={layer} "
-                        + f"hydra.run.dir=outputs/{ds}-{feat}-{seed}-{layer}"
-                    )
+    layers = [2, 4, 8]
+    seeds = [0]
+
+    combinations = list(
+        itertools.product(feature_set, data_source, layers, seeds)
+    )
+    for comb in combinations:
+        build_job(
+            f"dataset.data_source={comb[1]} "
+            + f"model.feature_set={comb[0]} "
+            + f"seed.seed={comb[3]} "
+            + f"model.aegis.n_layers={comb[2]} "
+            + f"hydra.run.dir=outputs/variants/{comb[0]}-{comb[1]}-{comb[2]}-{comb[3]}",
+            path_to_scripts,
+        )
 
 
 if __name__ == "__main__":
