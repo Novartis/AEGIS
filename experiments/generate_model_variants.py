9b8533dfda413af9d6f2446360ea0a4eb637bd74
Philip Hartout <philip.hartout@protonmail.com>
2023-05-18 12:21:38 +0200

    updates to compute reqs for model variants

--- experiments/generate_model_variants.py
+++ experiments/generate_model_variants.py
@@ -23,14 +23,16 @@ def build_job(overrides, path_to_scripts):
 
 # Give your job a name, so you can recognize it in the queue overview
 #SBATCH --job-name=imm
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=5
 #SBATCH --ntasks-per-core=2
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=10G
 #SBATCH --gres=shard:19
 #SBATCH --time=20-00:00:00
 #SBATCH --partition=p.hpcl91
-#SBATCH --exclude=hpcl9104,hpcl9101
+#SBATCH --exclude=hpcl9101,hpcl9104
 #SBATCH --output=/fs/pool/pool-hartout/Documents/Git/AEGIS/outputs/slurm_outputs/slurm-%j.out
+#SBATCH --mail-user=hartout@biochem.mpg.de 
+#SBATCH --mail-type=FAIL 
 source /fs/home/${"USER"}/.bashrc
 cd /fs/pool/pool-hartout/Documents/Git/AEGIS
 export CUDA_VISIBLE_DEVICES=0   
@@ -60,7 +62,7 @@ def main():
     feature_set = ["seq_only", "seq_mhc"]
     data_source = ["iedb", "iedb_nod", "nod"]
     layers = [2, 4, 8]
-    seeds = [0]
+    seeds = [0, 1, 2, 3]
 
     combinations = list(
         itertools.product(feature_set, data_source, layers, seeds)
