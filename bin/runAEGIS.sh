c5c220cef4cf42421056dcffee00bad99c729191
Philip Hartout <philip.hartout@protonmail.com>
2023-05-24 17:47:07 +0200

    remove unused bin/ directory

--- bin/runAEGIS.sh
+++
@@ -1,7 +0,0 @@
-_this_script=`readlink -f $0`
-_this_dir=`dirname $_this_script`
-python_env="/usr/prog/sb/mw/python/conda/sb-aegis/bin/python"
-
-$python_env ${_this_dir}/../src/mhciipresentation/evaluation/runAEGIS.py $@
-
-
