0499314d685ea5c67704532a3eb2f79930d264f8
Philip Hartout <philip.hartout@protonmail.com>
2023-05-31 11:22:58 +0200

    remove node restrictions, increase CPUs

--- experiments/generate_model_variants.py
+++ experiments/generate_model_variants.py
@@ -23,13 +23,12 @@ def build_job(overrides, path_to_scripts):
 
 # Give your job a name, so you can recognize it in the queue overview
 #SBATCH --job-name=imm
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=15
 #SBATCH --ntasks-per-core=2
 #SBATCH --mem-per-cpu=10G
 #SBATCH --gres=gpu:1
 #SBATCH --time=20-00:00:00
 #SBATCH --partition=p.hpcl91
-#SBATCH --exclude=hpcl9101,hpcl9104
 #SBATCH --output=/fs/pool/pool-hartout/Documents/Git/AEGIS/outputs/slurm_outputs/slurm-%j.out
 #SBATCH --mail-user=hartout@biochem.mpg.de 
 #SBATCH --mail-type=FAIL 
